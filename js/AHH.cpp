e5587b4ae6b465aab20bf28ef8c7fdb143ac0f1b
root <root@janick.localdomain>
2021-03-03 03:57:58 +0100

    more functions

--- js/AHH.cpp
+++ js/AHH.cpp
@@ -1,5 +1,6 @@
 #include "AHH.h"
 
+#include <renderer/font_renderer.h>
 
 EXPOSEC void* memset(void* buf, int c, int n){
 	unsigned char* p = (unsigned char*) buf;
@@ -51,4 +52,58 @@ EXPOSEC int strlen(char *src){
 	while (*src++)
 		i++;
 	return i;
+}
+
+EXPOSEC char *strcat(char *dest, const char *src){
+    int i;
+    int j;
+
+    for (i = 0; dest[i] != '\0'; i++);
+    for (j = 0; src[j] != '\0'; j++) {
+        dest[i+j] = src[j];
+    }
+
+    dest[i+j] = '\0';
+
+    return dest;
+}
+
+EXPOSEC double fabs(double num) {
+	if (num < 0) return -num;
+	return num;
+}
+
+EXPOSEC double sqrt(double num) {
+	double i = num / 2;
+	if (num < 0) return 0;
+	while (fabs(i - (num / i)) / i > 0.000000000000001) {
+		i = (i + (num / i)) / 2;
+	}
+	return i;
+}
+
+EXPOSEC double ceil(double num) {
+	if (num < 0) return (int) num;
+	return ((int) num) + 1;
+}
+
+EXPOSEC double floor(double x) {
+	if (x > 0) return (int) x;
+	return (int)(x - 0.9999999999999999);
+}
+
+EXPOSEC double fmod(double a, double b) {
+	return (int)((((a / b) - ((int)(a / b))) * b) + 0.5);
+}
+
+EXPOSEC double pow(double x,double y) {
+    double result = 1;
+    for (int i=0;i<y;i++)
+        result *= x;
+    return result;
+}
+
+EXPOSEC void __assert_fail(const char* __assertion, const char* __file, unsigned int __line) {
+	renderer::global_font_renderer->printf("ASSERT: %s\nFAIL: %s line %d\n", __assertion);
+	while(1);
 }
--- js/AHH.h
+++ js/AHH.h
@@ -18,5 +18,21 @@ EXPOSEC void* memcpy(void* dest, const void* src, size_t n);
 EXPOSEC int strcmp(char *str1, char *str2);
 EXPOSEC char *strcpy(char *dest, const char *src);
 EXPOSEC int strlen(char *src);
+EXPOSEC char *strcat(char *dest, const char *src);
+EXPOSEC double fabs(double num);
+EXPOSEC double sqrt(double num);
+EXPOSEC double ceil(double num);
+EXPOSEC double floor(double x);
+EXPOSEC double fmod(double a, double b);
+EXPOSEC double pow(double x,double y);
+EXPOSEC void __assert_fail(const char* __assertion, const char* __file, unsigned int __line);
+
+#define assert(expr)														\
+	((void) sizeof ((expr) ? 1 : 0), __extension__ ({						\
+		if (expr)															\
+			;																\
+		else																\
+			__assert_fail (#expr, __FILE__, __LINE__);	\
+	}))
 
 #endif
